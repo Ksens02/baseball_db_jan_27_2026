5e8e3d80ab63ee065c69ab5012895fa071dd2b6e
Keller-Sens <200811951+Ksens02@users.noreply.github.com>
2026-02-16 15:47:50 +0000

    created a gradio app.py

---
+++ feb16/app.py
@@ -0,0 +1,35 @@
+import sqlite3
+import pandas as pd
+import gradio as gr 
+
+conn = sqlite3.connect('../baseball.db')
+cursor = conn.cursor()
+query = """
+    WITH top_hitters AS (SELECT nameFirst, nameLast
+    FROM batting INNER JOIN people
+    ON batting.playerID = people.playerID
+    WHERE teamID = 'PHI'
+    GROUP BY batting.playerID
+    ORDER by sum(HR) desc
+    LIMIT 10)
+
+    SELECT CONCAT(nameFirst,' ',nameLast) as player
+    FROM top_hitters
+    ORDER BY nameLast
+"""
+
+cursor.execute(query)
+records = cursor.fetchall()
+conn.close()
+
+players = []
+for record in records:
+    players.append(record[0])
+# with gr.Blocks() as iface:
+#     playerID = gr.Dropdown(choices = records(), interactive = True)
+#     HR = gr.Number(label = "Home Runs")
+#     playerID.change(fn = f, inputs = [playerID], outputs = [HR])
+
+    
+# iface.launch()
+print(players)
